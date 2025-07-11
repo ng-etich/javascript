3a539b86c3938ee927e1d460f976143115eb9dff
ng-etich <kibiwottemmanuel66@gmail.com>
2025-07-11 12:12:03 +0300

    revision

---
+++ loops.js
@@ -0,0 +1,6 @@
+let counter =0;
+
+while (counter >500){
+    console.log("counter is at:" +counter);
+    
+}
--- operators.js
+++ operators.js
@@ -18,6 +18,47 @@ console.log(5 == 5);
 console.log(5 === 5);
 
 console.log(!5);
-o
+
 console.log(!0);
+
+// let newAge=60;
+
+// console.log(++age);
+// operators in javascript
+// 1.arithmetic operators used for mathematical calculations examples + - * /%
+console.log(5/3);
+let dayNumber=32;
+if(dayNumber%4 ==0){
+    console.log("Alert! send your cut");
+    
+}
+// 2 assignment operators used to assign values to variables example =, +=, -=, \*=, /=, %=, ++, --
+dayNumber +=10//add 10 to dayNumber  dayNumber=dayNumber + 10 ,
+console.log(dayNumber);
+
+
+
+// 3comparison opearators used to compare values and return a boolean resutlt example ==, ===, !=, !==<, <, >, <=, >=
+console.log(5 == '5');//loose quality checks only value
+console.log(5 ==="5");//strictly equality checks both the value and type
+console.log(32>20);
+
+
+let newAge =29;
+ if(newAge>21){
+    console.log("Allowed into the website");
+    
+ }
+let location ="Nairobi";
+// 4 logical operators used to perform logical operations and boolean values example &&(AND), ||(OR), !(NOT)
+if (newAge>31||  location =="Nairobi"){
+    console.log("Allowed into the website");
+    
+}
+
+// 5 ternary operators a shorthand for conditional statements condition ? valueIfTrue: valueIfFalse:
+
+
+
+
 
